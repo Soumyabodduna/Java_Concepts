9c69a6650fdeb4d12bd0ccbf7d7b9bbe3472eb60
sbodduna <ssoumya305@gmail.com>
2022-02-19 17:39:54 +0530

    update code

--- src/com/java/polymorphism/Overloading.java
+++ src/com/java/polymorphism/Overloading.java
@@ -1,31 +1,30 @@
 package com.java.polymorphism;
 
-
-class Arithimatic {
-    public int sum(int x, int y) {
-        return (x + y);
-    }
-
-    // Overloaded sum(). This sum takes three int parameters
-    public int sum(int x, int y, int z) {
-        return (x + y + z);
-    }
-
-    // Overloaded sum(). This sum takes two double parameters
-    public double sum(double x, double y) {
-        return (x + y);
-    }
-
-    // Class 2
-// Main class
-  //  public class Overloading {
-        // Main driver method
-        //public static void main(String[] args) {
-           // Arithimatic s = new Arithimatic();
-           // System.out.println(s.sum(10, 20));
-           // System.out.println(s.sum(10, 20, 30));
-          //  System.out.println(s.sum(10.5, 20.5));
+   public  class Overloading {
+        public static  int sum(int x, int y)
+        {
+            return (x + y);
         }
-   // }
-//}
+
+        // Overloaded sum(). This sum takes three int parameters
+        public static  int sum(int x, int y, int z)
+        {
+            return (x + y + z);
+        }
+
+        // Overloaded sum(). This sum takes two double parameters
+        public static  double sum(double x, double y)
+        {
+            return (x + y);
+        }
+        // Main driver method
+        public static void main(String[] args) {
+            Overloading s = new Overloading();
+             System.out.println(s.sum(10, 20));
+             System.out.println(s.sum(10, 20, 30));
+             System.out.println(s.sum(10.5, 20.5));
+        }
+
+    }
+
 
